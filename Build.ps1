62b4c30abd56d88ceaf4417646f943d372d4aa81
Einar Stenberg <einar@Ap3x.org>
2019-01-30 14:26:14 +0100

    Added build file for building docker image locally and RUNTHIS for basic setup with pullserver and sql express in separate containers

---
+++ Build.ps1
@@ -0,0 +1,21 @@
+﻿#Run this to build docker image PullserverContainer
+
+#copying files and create directories
+New-Item -Name PullServerContainer -ItemType Directory
+New-Item -Name Modules -ItemType Directory -Path .\PullServerContainer
+Install-Module -Name xPSDesiredStateConfiguration
+Copy-Item -Path 'C:\Program Files\WindowsPowerShell\Modules\xPSDesiredStateConfiguration' -Destination .\PullServerContainer\Modules -Recurse -Force
+Copy-Item .\Docker.ps1 .\PullServerContainer -Force
+Copy-Item .\DockerMon.ps1 .\PullServerContainer -Force
+Copy-Item .\sqlncli.msi .\PullServerContainer -Force
+Copy-Item .\DockerFile .\PullServerContainer -Force
+
+#Actual build
+docker build .\PullServerContainer -t einar/dscstack:latest
+
+#Cleanup
+Remove-Item .\PullServerContainer -Force
+
+#Push image to docker hub
+#docker login
+#docker image push einar/dscstack
---
+++ RUNTHIS.ps1
@@ -0,0 +1,39 @@
+﻿
+
+
+
+#input your owna variables, especially password
+$SQLSAPassword="Topsecret!"
+$SQLDbName="DSC"
+$SQLServerName="localhost"
+$SQLDir="E:\sqlpullserver"  #Use double \\
+$PullServerDir="E:\pullserver"
+$PullServerPort="8080"
+
+
+
+
+
+#Create SQL connection string from user variables
+$SQLString="Provider=SQLNCLI11;Server=$SQLServerName;Database=$SQLDbName;User ID=SA;Password=$SQLSAPassword;Initial Catalog=master;"
+
+
+#Check if docker is installed, if not run install.
+
+#
+#
+#
+
+
+
+#Create docker container for sql
+docker run --isolation hyperv -d -p 1433:1433 -e sa_password=$SQLSAPassword -e ACCEPT_EULA=Y -v $SQLDir`:c:\temp\ -e attach_dbs="[{'dbName':'$SQLDbName','dbFiles':['c:\\temp\\$SQLDbName.mdf','c:\\temp\\$SQLDbName`_log.ldf']}]" microsoft/mssql-server-windows-express
+
+
+#create docker container for DSC
+docker run --isolation hyperv `
+    -p $PullServerPort`:8080 `
+    -v $PullServerDir`:C:\pullserver `
+    -e ConnectionString="$SQLString" `
+    --rm `
+    -d einar/dscstack
